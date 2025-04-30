e687195b927978ac51ec70cacc055ddadd9f3104
Sara-Hesham-Mohammed <142182302+Sara-Hesham-Mohammed@users.noreply.github.com>
2025-05-01 01:20:26 +0300

    final files for clustering and embedding

--- API/API.py
+++ API/API.py
@@ -1,20 +1,12 @@
-import pickle
-
+import json
 import pandas as pd
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from API.User_Model import User
 from typing import List
 from Clusters import user_embedding
 from Clusters.GaussianClusters import dimensionality_reduction, get_clusters
 from Clusters.user_embedding import create_user_text
-
-
-def get_model(path):
-    with open(path, 'rb') as f:
-        model = pickle.load(f)
-        return model
-
 app = FastAPI()
 #to be able to send requests using html file?
 app.add_middleware(
@@ -27,29 +19,26 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return {"message": "This is the python backend for the recommendation system and group formation."}
+    return {"message": "This is the python backend for the embeddings and clustering(group formation)."}
 
-@app.post("/recommendation/")
-async def get_recommendation(user):
-    # PASS THE USER STUFF IN THE PARAMS
-    # call the graph rec model here?
-    model = get_model("models/recommendation.pth")
-    user = 'smth'
-    #prediction = model.predict(user)
-    prediction = ['x','y','z']
-    return {"Prediction": f"Get user info and return the recommendations for that user, prediction is {prediction[1]}"}
-
-@app.post("/store-embedding/")
+#### EMBEDDING ENDPOINT ####
+@app.post("/get-embedding/")
 async def store_embedding(user: User):
-    usr_text = create_user_text(user);
+    # serialise the user pydantic object to a dictionary
+    user_dict = user.model_dump()
+    usr_text = create_user_text(user_dict)
     embedding = user_embedding.get_embedding(usr_text)
     #this is what will get sent to the database service API (node.js)
-    return {"embedding": embedding.tolist()}
+    json_data = json.dumps({"embedding": embedding.tolist()})
+    return json_data
 
+#### CLUSTERING ENDPOINT ####
 @app.post("/get-group/")
 async def form_groups(users: List[User]):
     user_df = pd.DataFrame(users)
-    reduced_df = dimensionality_reduction(user_df)
+    embeddings_df = user_df['embedding']
+    reduced_df = dimensionality_reduction(embeddings_df)
     # Perform GMM clustering (soft clustering)
+    # pass the number of clusters as a parameter, default is 5
     groups = get_clusters(reduced_df)
     return groups
--- Clusters/GaussianClusters.py
+++ Clusters/GaussianClusters.py
@@ -1,25 +1,22 @@
 from sklearn.decomposition import PCA
 from sklearn.mixture import GaussianMixture
-from user_embedding import final_df
 
-embeddings_df = final_df.drop(columns=['id', 'age','tags'])  # Get the embeddings columns
-
-def dimensionality_reduction(users_df):
+def dimensionality_reduction(users_embeddings_df):
     # Check if the DataFrame is empty
-    if users_df.empty:
+    if users_embeddings_df.empty:
         raise ValueError("The embeddings DataFrame is empty.")
 
     # Check if the DataFrame has more than 1 column
-    if users_df.shape[1] <= 1:
+    if users_embeddings_df.shape[1] <= 1:
         raise ValueError("The embeddings DataFrame must have more than one column for dimensionality reduction.")
 
     # Dimensionality Reduction (PCA) for better clustering
     pca = PCA(n_components=10)  # Reduce dimensions to 10
-    reduced_embeddings = pca.fit_transform(users_df)
+    reduced_embeddings = pca.fit_transform(users_embeddings_df)
 
     return reduced_embeddings
 
-def get_clusters(reduced_embeddings):
+def get_clusters(reduced_embeddings, num_clusters=5):
     # Check if the reduced embeddings are empty
     if reduced_embeddings.size == 0:
         raise ValueError("The reduced embeddings are empty.")
@@ -29,18 +26,8 @@ def get_clusters(reduced_embeddings):
         raise ValueError("The reduced embeddings must have more than one dimension for clustering.")
 
     # Perform GMM clustering (soft clustering)
-    n_clusters = 5  # figure out way to make it dynamic according to how many users there are
+    n_clusters = num_clusters  # figure out way to make it dynamic according to how many users there are
     gmm = GaussianMixture(n_components=n_clusters, random_state=42)
     clusters = gmm.fit_predict(reduced_embeddings)
 
     return clusters
-
-# Perform dimensionality reduction
-reduced_embeddings = dimensionality_reduction(embeddings_df)
-# Perform GMM clustering
-clusters = get_clusters(reduced_embeddings)
-# Add the cluster labels to the original DataFrame
-final_df['cluster'] = clusters
-
-# Check the clustering result
-print(final_df[['id', 'age', 'tags', 'cluster']])
